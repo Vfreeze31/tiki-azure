8a138da097a838d47a703cc1050c90bf7244bc9a
Iestyn Best <Vfreeze31@users.noreply.github.com>
2017-02-24 16:49:42 +1100

    Update local.php

--- db/local.php
+++ db/local.php
@@ -17,17 +17,3 @@ foreach ($_SERVER as $key => $value) {
     $user_tiki = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
     $pass_tiki = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
 }
-
-$link = mysqli_connect($host_tiki, $user_tiki, $pass_tiki, $dbs_tiki);
-
-if (!$link) {
-    echo "Error: Unable to connect to MySQL." . PHP_EOL;
-    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-    exit;
-}
-
-echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
-echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
-
-mysqli_close($link);
